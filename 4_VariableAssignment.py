13e08809a56121f1cce7eec4d53edaf3a91ba3d7
AMAN HANS <40731663+amanhans306@users.noreply.github.com>
2018-07-05 21:42:55 -0700

    Update and rename 4_VariableAssignment to 4_VariableAssignment.py

--- 4_VariableAssignment.py
+++ 4_VariableAssignment.py
@@ -2,6 +2,8 @@
 # Author name 
 # Creation date 
 
+# Use jupyter notebook
+
 # Intialize variable and pass value  
 # Perfom arthmetic operation by using that variables 
-# Also find the type of variable value 
+# Also find the type of variable 
