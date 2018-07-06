348b3ba513a868fbe362c1d1c30a6987c971cc42
AMAN HANS <40731663+amanhans306@users.noreply.github.com>
2018-07-05 22:03:31 -0700

    Create 5_Strings.py

---
+++ 5_Strings.py
@@ -0,0 +1,8 @@
+# Author Name
+# Creation Date
+
+# Use what you know about the print() function to print out the phrase "Hello World". Make sure your capitalization and spacing mathches
+# Write a string index that returns just the letter 'r' from "Hello World" 
+# Use string slicing to grab the word 'ink' from inside "tinker"
+# Grab everything, but go in step sizes of 2 from "abcedefghijk"
+#  print a string backwards ; eg: "helloworld"
