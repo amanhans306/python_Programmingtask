5032ce1ea5c0c7abd4c92403bde329acc65d82be
Aman Hans <aman.36-cse-15@mietjammu.in>
2018-07-09 14:39:17 -0700

    Make cahnges

--- 5_Strings.py
+++ 5_Strings.py
@@ -5,4 +5,6 @@
 # Write a string index that returns just the letter 'r' from "Hello World" 
 # Use string slicing to grab the word 'ink' from inside "tinker"
 # Grab everything, but go in step sizes of 2 from "abcedefghijk"
-#  print a string backwards ; eg: "helloworld"
+# print a string backwards ; eg: "helloworld"
+# change the first letter of  word "Sam " to "R", so as to make it "Ram"
+# Use built in function to Upper case ,Lower case and Split the string"Hello World" 
